fee0a0fe446b4fee948c84cdb2d8844c3792a6dc
akashpat <akashpat@DIN73003014.corp.capgemini.com>
2018-08-06 12:10:29 +0530

    done 3

--- Day-1-Git_demo/src/main/java/org/cap/demo/MainClass.java
+++ Day-1-Git_demo/src/main/java/org/cap/demo/MainClass.java
@@ -3,7 +3,7 @@ package org.cap.demo;
 public class MainClass {
 	
 	public static void main(String[] args) {
-		
+		SalesClass s=new SalesClass();
 		System.out.println("hello");
 		System.out.println("Shubendu");
 	}
---
+++ Day-1-Git_demo/src/main/java/org/cap/demo/SalesClass.java
@@ -0,0 +1,11 @@
+package org.cap.demo;
+
+public class SalesClass {
+	private static void main(String[] args) {
+int []arr= {1,2,3,-98};
+for(int a:arr)
+{
+	System.out.println(a);
+}
+}
+}
